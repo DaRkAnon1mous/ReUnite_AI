d8a3a92a989519caee06df2a3d757ad77e878879
Shrey Dikshant <shreydikshant144@example.com>
2025-12-13 20:49:56 +0530

    Rewind

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -6,7 +6,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
     "http://localhost:5173",
+    # "http://localhost:5174",
     "http://127.0.0.1:5173",
+    # "http://127.0.0.1:5174",
     "https://darkanon1mous-reunite-ai.hf.space/",
     "https://reuniteai.netlify.app/"
 ]
